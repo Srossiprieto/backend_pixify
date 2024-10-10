df360ace524f9571e87cf3b500dae568261c6fd1
Santino <srossiprieto@gmail.com>
2024-10-10 17:14:51 -0300

    feat: deploy

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from './routes/users.routes'; 
+import cors from 'cors';
+import userRoutes from './routes/users.routes';
 import productRoutes from './routes/products.routes';
 import categoryRoutes from './routes/category.routes';
+
 dotenv.config();
 
 const app = express();
 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
-app.use('/api/categories', categoryRoutes); 
+app.use('/api/categories', categoryRoutes);
 
 export default app;
