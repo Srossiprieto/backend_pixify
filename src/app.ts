e0e79cdb20d41f85158683bc951e8e370d6017b2
Santino <srossiprieto@gmail.com>
2024-10-15 14:25:08 -0300

    feat: auth

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import userRoutes from './routes/users.routes';
 import productRoutes from './routes/products.routes';
 import categoryRoutes from './routes/category.routes';
+import authRoutes from './routes/auth.routes';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -11,13 +12,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app;
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
-  phone: { type: String, required: true},
-  password_hash: { type: String, required: true, select: false },
-}, {
-  timestamps: true 
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, trim: true},
+    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+    password: { type: String, required: true, select: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
