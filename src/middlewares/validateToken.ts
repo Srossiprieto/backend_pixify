efb2088dde9ff1b82faa508aa827648db19e4d0d
Santino <srossiprieto@gmail.com>
2024-10-19 19:32:09 -0300

    feat: changes1

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -1,22 +1,30 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
 
-interface JwtPayload {
-  id: string;
-}
-
-export const authRequired = (req: Request, res: Response, next: NextFunction) => {
+export const authRequired = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
   const { token } = req.cookies;
 
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ message: "Unauthorized" });
+    return; // Detenemos la ejecución de la función, pero no devolvemos el Response
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
-    req.user = decoded;
-    next();
+    const decoded = jwt.verify(token, JWT_SECRET as string) as jwt.JwtPayload;
+
+    // Verificamos si el payload tiene un "id"
+    if (decoded?.id) {
+      req.user = { id: decoded.id };
+      next();
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
   } catch (err) {
-    return res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ message: "Unauthorized" });
   }
 };
