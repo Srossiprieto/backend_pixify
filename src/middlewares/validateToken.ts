3e4e00b4f81e65863b03d876a32c1edb5cd3c38b
Santino <srossiprieto@gmail.com>
2024-10-21 11:03:09 -0300

    feat: authContext

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -1,21 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-interface JwtPayload {
+// Interfaz para el payload del JWT
+interface CustomJwtPayload extends JwtPayload {
   id: string;
 }
 
-export const authRequired = (req: Request, res: Response, next: NextFunction): void => {
-  const { token } = req.cookies;
 
+export const authRequired = (req: Request, res: Response, next: NextFunction): void => {
+  // Accede correctamente a la cookie "token"
+  const token = req.cookies.token;
+  // Si no hay token, retorna un 401
   if (!token) {
     res.status(401).json({ message: "Unauthorized" });
+    console.log("No hay token");
     return;
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+    // Verifica el token usando el secreto
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as CustomJwtPayload;
+    
+    // Almacena el payload decodificado en req.user
     req.user = decoded;
+
+    // Continua con el siguiente middleware
     next();
   } catch (err) {
     res.status(401).json({ message: "Unauthorized" });
